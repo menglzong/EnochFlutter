1b3659603693185c164b769a221960ff78492679
enochzong <menglzong>
2022-12-24 20:30:06 +0800

    tab

---
+++ flutter_ios/lib/src/center/index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CenterPage extends StatefulWidget {
+  const CenterPage({Key? key}) : super(key: key);
+
+  @override
+  State<CenterPage> createState() {
+    return _CenterPageState();
+  }
+}
+
+class _CenterPageState extends State<CenterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('hkhkjhkhkhk')));
+  }
+}
---
+++ flutter_ios/lib/src/home/index.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('首页'),
+      ),
+      body: Center(child: Text('sdfsdfsd')),
+    );
+  }
+}
